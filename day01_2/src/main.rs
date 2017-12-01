2808ad008f79501b8cd35cdb55e217402179b7b2
panicbit <panicbit.dev@gmail.com>
2017-12-01 06:20:00 +0100

    Day 1

---
+++ day01_2/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let input = include_str!("../input.txt").trim();
+    let mut sum = 0;
+    let digits: Vec<u32> = input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    for i in 0..digits.len() {
+        let j = (i + digits.len() / 2) % digits.len();
+        if digits[i] == digits[j] {
+            sum += digits[i];
+        }
+    }
+
+    println!("{}", sum);
+}
