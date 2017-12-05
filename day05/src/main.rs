7142502ebb9270b4902d7e2d4ecaa70da76ecfb3
panicbit <panicbit.dev@gmail.com>
2017-12-05 14:46:09 +0100

    Day 5

---
+++ day05/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    let mut ip: i32 = 0;
+    let mut steps = 0;
+    let mut instructions = input
+        .lines()
+        .map(|line| line.trim().parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    while let Some(instruction) = instructions.get_mut(ip as usize) {
+        ip += *instruction as i32;
+        steps += 1;
+        *instruction += 1;
+    }
+
+    println!("{}", steps);
+}
