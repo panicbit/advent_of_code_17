21099fd259169fbd193ec4a2189a0a626a53caff
panicbit <panicbit.dev@gmail.com>
2017-12-06 06:35:34 +0100

    Day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,7 +1,43 @@
 #[macro_use]
 extern crate aoc;
 
-aoc!(2017, 06, 1, |input| {
-    unimplemented!();
-    0
+use std::collections::HashSet;
+
+aoc!(2017, 6, 1, |input| {
+    let mut count = 0;
+    let mut visited = HashSet::new();
+    let mut banks = input
+        .trim()
+        .split('\t')
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    visited.insert(banks.clone());
+
+    loop {
+        let max = *banks.iter().max().unwrap();
+        let bank_index = banks.iter().position(|&n| n == max).unwrap();
+        let mut blocks = banks[bank_index];
+        let num_banks = banks.len();
+        banks[bank_index] = 0;
+
+        for i in (0..num_banks).cycle().skip(bank_index+1) {
+            if blocks == 0 {
+                break;
+            }
+
+            banks[i] += 1;
+            blocks -= 1;
+        }
+
+        count += 1;
+
+        if visited.contains(&banks) {
+            break;
+        }
+
+        visited.insert(banks.clone());
+    }
+
+    count
 });
--- day06_2/src/main.rs
+++ day06_2/src/main.rs
@@ -1,7 +1,49 @@
 #[macro_use]
 extern crate aoc;
 
-aoc!(2017, 06, 2, |input| {
-    unimplemented!();
-    0
+use std::collections::HashSet;
+
+aoc!(2017, 6, 2, |input| {
+    let mut count = 0;
+    let mut visited = HashSet::new();
+    let mut banks = input
+        .trim()
+        .split('\t')
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let mut initial_run = true;
+
+    visited.insert(banks.clone());
+
+    loop {
+        let max = *banks.iter().max().unwrap();
+        let bank_index = banks.iter().position(|&n| n == max).unwrap();
+        let mut blocks = banks[bank_index];
+        let num_banks = banks.len();
+        banks[bank_index] = 0;
+
+        for i in (0..num_banks).cycle().skip(bank_index+1) {
+            if blocks == 0 {
+                break;
+            }
+
+            banks[i] += 1;
+            blocks -= 1;
+        }
+
+        count += 1;
+
+        if initial_run && visited.contains(&banks) {
+            visited.clear();
+            count = 0;
+            initial_run = false;
+        }
+        else if !initial_run && visited.contains(&banks) {
+            break;
+        }
+
+        visited.insert(banks.clone());
+    }
+
+    count
 });
