a5f4e217adb91d7d0cb4e75ff78cd21499ebdf89
panicbit <panicbit.dev@gmail.com>
2017-12-06 02:45:52 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+set -x
+
+event=2017
+start=1
+end=25
+
+for day in `seq -f "%02.f" $start $end`; do
+    for part in `seq 1 2`; do
+        if [ "$part" == "1" ]; then
+            project="day${day}"
+        else
+            project="day${day}_${part}"
+        fi
+
+        cargo new --bin "$project"
+        (
+            cd "$project"
+            cargo add aoc --path ../aoc
+            cat << EOF > src/main.rs
+#[macro_use]
+extern crate aoc;
+
+aoc!($event, $day, $part, |input| {
+    unimplemented!();
+    0
+});
+EOF
+        )
+    done
+done
