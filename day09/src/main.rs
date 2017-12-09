8ff0ad1684655ceefd7cadc796288e5947ebfaad
panicbit <panicbit.dev@gmail.com>
2017-12-09 06:23:27 +0100

    Day 9

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,7 +1,33 @@
 #[macro_use]
 extern crate aoc;
 
+use self::Mode::*;
+
 aoc!(2017, 09, 1, |input| {
-    unimplemented!();
-    0
+    let mut score = 0;
+    let mut level = 0;
+    let mut chars = input.chars();
+    let mut mode = Normal;
+
+    while let Some(ch) = chars.next() {
+        match (mode, ch) {
+            (Normal, '{') => level += 1,
+            (Normal, '}') => {
+                score += level;
+                level -= 1;
+            },
+            (Normal, '<') => mode = Garbage,
+            (Garbage, '!') => { chars.next(); },
+            (Garbage, '>') => mode = Normal,
+            _ => {},
+        }
+    }
+
+    score
 });
+
+#[derive(Copy,Clone)]
+enum Mode {
+    Normal,
+    Garbage,
+}
--- day09_2/src/main.rs
+++ day09_2/src/main.rs
@@ -1,7 +1,28 @@
 #[macro_use]
 extern crate aoc;
 
+use self::Mode::*;
+
 aoc!(2017, 09, 2, |input| {
-    unimplemented!();
-    0
+    let mut garbage = 0;
+    let mut chars = input.chars();
+    let mut mode = Normal;
+
+    while let Some(ch) = chars.next() {
+        match (mode, ch) {
+            (Normal, '<') => mode = Garbage,
+            (Garbage, '!') => { chars.next(); },
+            (Garbage, '>') => mode = Normal,
+            (Garbage, _) => garbage += 1,
+            _ => {},
+        }
+    }
+
+    garbage
 });
+
+#[derive(Copy,Clone)]
+enum Mode {
+    Normal,
+    Garbage,
+}
