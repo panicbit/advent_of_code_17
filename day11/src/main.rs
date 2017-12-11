40256b4a21ff7a887e9f1f58f015264b0c2446e2
panicbit <panicbit.dev@gmail.com>
2017-12-11 11:06:30 +0100

    Day 11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,7 +1,60 @@
 #[macro_use]
 extern crate aoc;
 
+use std::cmp::max;
+
 aoc!(2017, 11, 1, |input| {
-    unimplemented!();
-    0
+    let steps = input.trim().split(',').map(Dir::from_str);
+    let mut pos = HexPos::default();
+    
+    for step in steps {
+        pos.step(step);
+    }
+    
+    max(pos.n.abs(), pos.se.abs())
 });
+
+#[derive(Default)]
+struct HexPos {
+    n: i32,
+    se: i32,
+}
+
+impl HexPos {
+    fn step(&mut self, dir: Dir) {
+        match dir {
+            Dir::N => self.n += 1,
+            Dir::S => self.n -= 1,
+            Dir::SE => self.se += 1,
+            Dir::NW => self.se -= 1,
+            Dir::NE => {
+                self.n += 1;
+                self.se += 1;
+            },
+            Dir::SW => {
+                self.n -= 1;
+                self.se -= 1;
+            },
+        }
+    }
+} 
+
+enum Dir {
+    N, S,
+    NW, SE,
+    SW, NE,
+}
+
+impl Dir {
+    fn from_str(dir: &str) -> Dir {
+        match dir {
+            "n" => Dir::N,
+            "s" => Dir::S,
+            "nw" => Dir::NW,
+            "se" => Dir::SE,
+            "sw" => Dir::SW,
+            "ne" => Dir::NE,
+            other => panic!("Unknown dir {:?}", other),
+        }
+    }
+}
--- day11_2/src/main.rs
+++ day11_2/src/main.rs
@@ -1,7 +1,62 @@
 #[macro_use]
 extern crate aoc;
 
+use std::cmp::max;
+
 aoc!(2017, 11, 2, |input| {
-    unimplemented!();
-    0
+    let steps = input.trim().split(',').map(Dir::from_str);
+    let mut pos = HexPos::default();
+    let mut max_dist = 0;
+
+    for step in steps {
+        pos.step(step);
+        max_dist = max(max_dist, max(pos.n.abs(), pos.se.abs()));
+    }
+    
+    max_dist
 });
+
+#[derive(Default,Copy,Clone)]
+struct HexPos {
+    n: i32,
+    se: i32,
+}
+
+impl HexPos {
+    fn step(&mut self, dir: Dir) {
+        match dir {
+            Dir::N => self.n += 1,
+            Dir::S => self.n -= 1,
+            Dir::SE => self.se += 1,
+            Dir::NW => self.se -= 1,
+            Dir::NE => {
+                self.n += 1;
+                self.se += 1;
+            },
+            Dir::SW => {
+                self.n -= 1;
+                self.se -= 1;
+            },
+        }
+    }
+} 
+
+enum Dir {
+    N, S,
+    NW, SE,
+    SW, NE,
+}
+
+impl Dir {
+    fn from_str(dir: &str) -> Dir {
+        match dir {
+            "n" => Dir::N,
+            "s" => Dir::S,
+            "nw" => Dir::NW,
+            "se" => Dir::SE,
+            "sw" => Dir::SW,
+            "ne" => Dir::NE,
+            other => panic!("Unknown dir {:?}", other),
+        }
+    }
+}
