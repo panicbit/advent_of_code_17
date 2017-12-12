79103036c66d6c06e164a4adf7f9f46baef92282
panicbit <panicbit.dev@gmail.com>
2017-12-12 06:44:36 +0100

    Day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,7 +1,37 @@
 #[macro_use]
 extern crate aoc;
 
+use std::collections::{HashMap,HashSet};
+
 aoc!(2017, 12, 1, |input| {
-    unimplemented!();
-    0
+    let pipes = input.lines().map(|line| {
+        let mut line = line.trim().split(" <-> ");
+        let first = parse_int(line.next().unwrap());
+        let others = line.next().unwrap()
+            .split(", ")
+            .map(parse_int)
+            .collect::<Vec<_>>();
+        (first, others)
+    }).collect::<HashMap<_,_>>();
+
+    let mut visited = HashSet::new();
+    visit_group(0, &pipes, &mut visited);
+
+    visited.len()
 });
+
+fn visit_group(program: i32, pipes: &HashMap<i32, Vec<i32>>, visited: &mut HashSet<i32>) {
+    if visited.contains(&program) {
+        return;
+    }
+
+    visited.insert(program);
+
+    for &program in &pipes[&program] {
+        visit_group(program, pipes, visited);
+    }
+}
+
+fn parse_int(n: &str) -> i32 {
+    n.parse().unwrap()
+}
