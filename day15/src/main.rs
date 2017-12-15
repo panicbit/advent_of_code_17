a6478402e846acf30e89090a71f6750fa77cbda1
panicbit <panicbit.dev@gmail.com>
2017-12-15 06:55:49 +0100

    Simplify day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -2,12 +2,10 @@
 extern crate aoc;
 extern crate itertools;
 
-use itertools::iterate;
+use itertools::{Itertools, iterate};
 
 aoc!(2017, 15, 1, |input| {
-    let mut lines = input.lines();
-    let init_a = lines.next().unwrap()[24..].trim().parse::<u64>().unwrap();
-    let init_b = lines.next().unwrap()[24..].trim().parse::<u64>().unwrap();
+    let (init_a, init_b) = input.lines().map(|line| line[24..].trim().parse::<u64>().unwrap()).tuple_windows().next().unwrap();
     let gen_a = iterate(init_a, |last| last * 16807 % 2147483647);
     let gen_b = iterate(init_b, |last| last * 48271 % 2147483647);
 
