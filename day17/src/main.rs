e170827c3cac7f1160fa438d1457563d461326b3
panicbit <panicbit.dev@gmail.com>
2017-12-17 15:18:41 +0100

    Day 17

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -2,6 +2,14 @@
 extern crate aoc;
 
 aoc!(2017, 17, 1, |input| {
-    unimplemented!();
-    0
+    let dist = input.trim().parse::<usize>().unwrap();
+    let mut current_pos = 0;
+    let mut buf = vec![0];
+    
+    for num in 1..2018 {
+        current_pos = (current_pos + dist) % buf.len() + 1;
+        buf.insert(current_pos, num);
+    }
+
+    buf[(current_pos + 1) % buf.len()]
 });
--- day17_2/src/main.rs
+++ day17_2/src/main.rs
@@ -2,6 +2,18 @@
 extern crate aoc;
 
 aoc!(2017, 17, 2, |input| {
-    unimplemented!();
-    0
+    let dist = input.trim().parse::<usize>().unwrap();
+    let mut current_pos = 0;
+    let mut val1 = 0;
+    let mut len = 1;
+
+    for num in 1..(50000000) {
+        current_pos = (current_pos + dist) % len + 1;
+        if current_pos == 1 {
+            val1 = num;
+        }
+        len += 1;
+    }
+
+    val1
 });
